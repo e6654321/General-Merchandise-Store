49819b78a78e67812865c55e0b66ecb04760a4c1
Albert Pardillo <albertpardillo42@gmail.com>
2020-09-11 03:06:47 +0800

    albert's contribution

--- frontend/js/demo/datatables-demo.js
+++ frontend/js/demo/datatables-demo.js
@@ -1,4 +1,17 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    dom:'lBfrtip',
+    buttons: [
+      {
+          extend: 'excel',
+          text: 'Export to Excel',
+          exportOptions: {
+              modifier: {
+                  page: 'current'
+              }
+          }
+      }
+  ]
+  });
 });
